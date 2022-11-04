3f51ecdd63422a9c4c1d84a62e64343ef32432ae
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-11-04 23:28:59 +0300

    todo react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useState } from 'react'
+import TodoList from './TodoList'
 
 
 function App() {
-  return 
+  // for todos, the default state will be an empty array. then use obj destructuring
+  const [todos, setTodos] = useState([])
+  // you can't put 2 html or jsx elements next to eachother in a return. 
+  // The return can only return one thing. Any Js function returns only one thing.
+  // Wrap it inside an empty element(fragment) that allows us to return one thing
+  return (
+    
+   <>
+    <TodoList />
+    <input type = 'text' />
+    <button>Add Todo</button>
+    <button>Clear Completed</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
